7b12c6f0fddbf49bb0c457badbb3de7092d658f2
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-05-30 14:30:39 -0400

    Add home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,92 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import {  component$, useSignal, useStore, useTask$, useVisibleTask$ } from '@builder.io/qwik';
+import { type DocumentHead } from '@builder.io/qwik-city';
+import { Character } from '~/components/character/character';
+import type { Root, Data, CharacterType } from '~/models';
+
+interface storeProps{
+  page: number
+  data?: Data | null,
+  loading: boolean
+}
 
 export default component$(() => {
+ 
+  const state = useStore<storeProps>({
+    page: 0,
+    data: null,
+    loading: false
+  })
+
+  const element = useSignal<HTMLElement>();
+
+  useTask$(async ({track, cleanup}) => {
+    track(() => state.page)
+    const url = `http://localhost:5173/api/characters/${state.page}`
+    const controller = new AbortController()
+    cleanup(() => controller.abort())
+
+    try {
+      const respuesta = await fetch(url, {
+        method: 'GET',
+        signal: controller?.signal
+      })
+      const res = await respuesta.json() as Root;
+      console.log('res', res.data);
+
+      if(state.data){
+        state.data.results = [...state.data.results, ...res.data.results]
+      }else{
+        state.data = res.data
+      }
+      
+    } catch (error) {
+      console.error(error);
+    }
+  })
+
+  useVisibleTask$(({track, cleanup}) =>{
+    track(() => element.value)  
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0]?.isIntersecting) {
+          state.page = ++state.page;
+        }
+      },
+      { 
+        threshold: 0.1 
+      }
+    );
+    if (element?.value) {
+      observer.observe(element.value);
+    }
+
+    cleanup( () => observer.disconnect())
+  
+  })
+
   return (
     <>
-     <h1 class='text-3xl'>Hola</h1>
+      <header class='w-full flex items-center justify-center bg-red-500 p-8'>
+        <h1 class='text-7xl w-full inline-block text-center text-stone-50 '>Marvel Heroes</h1>
+      </header>
+      <section class='w-full flex justify-center items-center'>
+        <div class='p-8 w-full grid  gap-8 justify-between items-center '
+          style={{gridTemplateColumns:'repeat(auto-fill, minmax(10rem, 1fr))'}}>
+          {state.data && 
+            state.data.results?.length && 
+            state.data.results.map((result: CharacterType) => {
+            
+              return(
+                <Character
+                  ref={element}
+                  key={result.id}
+                  character={result}
+                />
+              )  
+            })}
+        </div>
+      </section>
     </>
   );
 });
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,7 @@
 import { component$, Slot } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
+import { Footer } from '~/components/footer';
+import { Header } from '~/components/header';
 
 
 export const useServerTimeLoader = routeLoader$(() => {
@@ -12,9 +14,11 @@ export default component$(() => {
 
   return (
     <>
-       <main>
+      <Header />
+       <main class='container mx-auto py-0'>
         <Slot />
       </main>
+      <Footer />
     </>
   );
 });
