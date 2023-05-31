6585af1a2ebd12eb0b0aba4bc451aaff3b94291d
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-05-30 21:03:23 -0400

    Create intersectionObserver hook for infinite scroll

---
+++ src/hooks/use-intersection-observer.tsx
@@ -0,0 +1,20 @@
+import { useSignal, useVisibleTask$ } from "@builder.io/qwik";
+
+export function useIntersectionObserver(state:{page:number}){
+  const element = useSignal<HTMLElement>()
+  useVisibleTask$(({ track, cleanup }) => {
+    track(() => element.value);
+  
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0]?.isIntersecting) state.page = ++state.page;
+      },
+      { threshold: 0.1 }
+    );
+    if (element?.value) observer.observe(element.value);
+    
+    cleanup(() => observer.disconnect());
+  });
+
+  return element;
+}
