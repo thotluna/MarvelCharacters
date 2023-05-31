62fd5547b9638ec0c49108075873aa3b62deb433
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-05-30 21:04:47 -0400

    Change Root at api

---
+++ src/routes/api/characters/[...all]/index.tsx
@@ -0,0 +1,23 @@
+import { type RequestHandler } from "@builder.io/qwik-city";
+import { generateUrl } from "~/services/fetchCharacters";
+
+export const onGet: RequestHandler = async (requestEvent) => {
+  const privateKey = requestEvent.env.get("API_TOKEN_KEY") ?? "";
+  const pathname = requestEvent.pathname
+  const page = Number(pathname.split("/").at(-2))
+  const search = pathname.split("/").at(-1)
+
+  const url = generateUrl(privateKey, page, search);
+
+  try {
+    console.log('Url remote', url);
+    
+    const res = await fetch(url, {
+      method: "GET",
+    });
+    const data = await res.json();
+    requestEvent.json(200, data);
+  } catch (error) {
+    requestEvent.json(500, { error: error });
+  }
+};
